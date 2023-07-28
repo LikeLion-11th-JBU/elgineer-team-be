f02d247f6d4d1bd11fa194a8c21926367886c7f1
승일 <seoung1515@naver.com>
2023-07-28 18:29:59 +0900

    [Add] Framework add

---
+++ hackerton-elgineer/src/main/java/com/elgineer/hackertonelgineer/HackertonElgineerApplication.java
@@ -0,0 +1,13 @@
+package com.elgineer.hackertonelgineer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackertonElgineerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackertonElgineerApplication.class, args);
+	}
+
+}
---
+++ hackerton-elgineer/src/test/java/com/elgineer/hackertonelgineer/HackertonElgineerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elgineer.hackertonelgineer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackertonElgineerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
