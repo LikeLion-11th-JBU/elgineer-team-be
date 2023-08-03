561b332941b3d1724fae95f093810bc8f7e0fd08
승일 <seoung1515@naver.com>
2023-08-04 01:53:21 +0900

    [Add] lifecategory main page demo

--- hackerton-elgineer/src/main/java/com/elgineer/hackertonelgineer/controller/LifeCategoryController.java
+++ hackerton-elgineer/src/main/java/com/elgineer/hackertonelgineer/controller/LifeCategoryController.java
@@ -1,4 +1,28 @@
 package com.elgineer.hackertonelgineer.controller;
 
+
+
+import com.elgineer.hackertonelgineer.dto.LifeCategoryDto;
+import com.elgineer.hackertonelgineer.entity.LifeCategory;
+import com.elgineer.hackertonelgineer.service.LifeCategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class LifeCategoryController {
+    @Autowired
+    private LifeCategoryService categoryService;
+
+    @GetMapping("/lifecategory")
+    public String showCategories(Model model) {
+        List<LifeCategory> categories = categoryService.getAllCategories();
+        model.addAttribute("categories", categories);
+        return "lifecategory.html"; // lifecategory.html을 템플릿으로 사용합니다.
+    }
+
 }
---
+++ hackerton-elgineer/src/main/java/com/elgineer/hackertonelgineer/entity/LifeCategory.java
@@ -0,0 +1,25 @@
+package com.elgineer.hackertonelgineer.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class LifeCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    // 생성자, getter, setter 등 필요한 코드 작성
+
+    // 기본 생성자
+    public LifeCategory() {
+    }
+
+    // 생성자
+    public LifeCategory(String name) {
+        this.name = name;
+    }
+}
