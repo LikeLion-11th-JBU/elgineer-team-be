3d24e47943c904a3ebc3db356a041ba3f23b716a
승일 <seoung1515@naver.com>
2023-08-08 16:03:20 +0900

    [Modification] main controller

--- hackerton-elgineer/src/main/java/com/elgineer/hackertonelgineer/controller/MainController.java
+++ hackerton-elgineer/src/main/java/com/elgineer/hackertonelgineer/controller/MainController.java
@@ -1,7 +1,9 @@
 package com.elgineer.hackertonelgineer.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class MainController {
     @GetMapping("/")
     public String main(){
