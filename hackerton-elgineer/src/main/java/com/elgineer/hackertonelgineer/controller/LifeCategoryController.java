b3d46f1b3f1869be927178f76a6177010cdbcf85
승일 <seoung1515@naver.com>
2023-08-14 21:16:53 +0900

    [Update] import

--- hackerton-elgineer/src/main/java/com/elgineer/hackertonelgineer/controller/LifeCategoryController.java
+++ hackerton-elgineer/src/main/java/com/elgineer/hackertonelgineer/controller/LifeCategoryController.java
@@ -1,11 +1,5 @@
 package com.elgineer.hackertonelgineer.controller;
 
-
-
-
-import com.elgineer.hackertonelgineer.entity.LifeCategory;
-import com.elgineer.hackertonelgineer.service.LifeCategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
